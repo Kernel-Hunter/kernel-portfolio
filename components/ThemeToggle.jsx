b79fe077ed180a0f39adb7f8c34425d7824d2b53
Karim Masmoudi <karim.masmoudi.pro@gmail.com>
2025-11-26 21:27:37 +0100

    Refactor ThemeToggle to use 'theme' from useTheme

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -4,8 +4,9 @@ import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
 export default function ThemeToggle() {
-  const { resolvedTheme, setTheme } = useTheme();
+  const { theme, setTheme, resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+
   useEffect(() => setMounted(true), []);
 
   if (!mounted) {
